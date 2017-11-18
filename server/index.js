b247e2a71a2222e0182a33b92d6b89eadd7705cd
Kelly Dwan <ryelle@users.noreply.github.com>
2017-11-17 23:14:45 -0500

    Serve an SVG to name or hex-based URLs

--- server/index.js
+++ server/index.js
@@ -3,15 +3,17 @@
  */
 import Express from 'express';
 import React from 'react';
+import { find } from 'lodash';
 import { renderToString } from 'react-dom/server';
 import path from 'path';
 
 /**
  * Internal dependencies
  */
-// import App from '../src/App';
-import Skeleton from '../src/components/skeleton';
 import assets from '../build/asset-manifest.json';
+import Flag from '../src/components/flag';
+import Skeleton from '../src/components/skeleton';
+import { list } from '../src/utils/colors';
 
 const app = Express();
 app.set('view engine', 'pug');
@@ -21,10 +23,49 @@ app.use('/static', Express.static(path.resolve(__dirname, '../build/static')));
 const pageTitle = 'Queeromattic Flags';
 
 app.get('/', function(req, res) {
-	const markup = renderToString( <Skeleton /> );
+	const markup = renderToString(<Skeleton />);
 	const jsFile = '/' + assets['main.js'];
 	const cssFile = '/' + assets['main.css'];
 	return res.render('index', { pageTitle, cssFile, jsFile, markup });
+});
+
+function buildSVG(colors) {
+	const markup = renderToString(<Flag colors={colors} />);
+	const headers =
+		'<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
+		'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+	return (
+		headers +
+		markup.replace(
+			'<svg ',
+			'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" ',
+		)
+	);
+}
+
+app.get('/hex/:colors.svg', function(req, res) {
+	const { colors = '' } = req.params;
+	if (-1 !== colors.indexOf('-')) {
+		const list = colors.split('-');
+		res.setHeader('Content-Type', 'image/svg+xml');
+		return res.send(buildSVG(list.map(item => '#' + item)));
+	}
+	res.status(400).send("Sorry, I don't understand your colors request!");
+});
+
+app.get('/name/:name.svg', function(req, res) {
+	const { name = '' } = req.params;
+	const { colors = [] } = find(list, { value: name });
+	if (colors.length) {
+		res.setHeader('Content-Type', 'image/svg+xml');
+		return res.send(buildSVG(colors));
+	}
+	res
+		.status(400)
+		.send(
+			`Sorry, I don't have ${name} on file.` +
+				'Maybe you should <a href="https://github.com/ryelle/queeromattic-flags">make a suggestion on the repo</a>?',
+		);
 });
 
 var server = app.listen(3000, function() {
