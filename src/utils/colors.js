de31a8b972bc96e687f14e53976d103d4869db1f
Kelly Dwan <ryelle@users.noreply.github.com>
2019-11-21 22:18:21 -0500

    Add second lesbian flag

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -40,7 +40,7 @@ const PAN = [ '#D71673', '#FDB813', '#3EA7D5' ];
 const POLY = [ '#D71673', '#74B95C', '#219CD3' ];
 const TRANS = [ '#61CDF6', '#F7ABB8', '#F4F4F5', '#F7ABB8', '#61CDF6' ];
 const PLACEHOLDER = '#C8C8CC';
-const LES = [
+const LESBIAN_LP = [
 	'#A40061',
 	'#B75592',
 	'#D063A6',
@@ -48,6 +48,15 @@ const LES = [
 	'#E4ACCF',
 	'#C54E54',
 	'#8A1E04',
+];
+const LESBIAN = [
+	'#DF4C3E',
+	'#F47C3B',
+	'#FDB813',
+	'#F4F4F5',
+	'#EC77A0',
+	'#B75592',
+	'#921E79',
 ];
 
 export const list = [
@@ -87,9 +96,14 @@ export const list = [
 		colors: [],
 	},
 	{
+		label: 'Lesbian (lipstick)',
+		value: 'lesbian-lipstick',
+		colors: LESBIAN_LP,
+	},
+	{
 		label: 'Lesbian',
 		value: 'lesbian',
-		colors: LES,
+		colors: LESBIAN,
 	},
 	{
 		label: 'LGBTQ #MoreColorMorePride',
